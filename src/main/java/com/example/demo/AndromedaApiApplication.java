c0b243d8d366cc16a7ab9797829b5e06fbecd3b8
Fran <franbl98@gmail.com>
2022-02-08 08:52:02 +0100

    First commit creando proyecto

---
+++ src/main/java/com/example/demo/AndromedaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndromedaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndromedaApiApplication.class, args);
+	}
+
+}
