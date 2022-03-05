5e194f522375b7e77bc93bc371d0aad92cbdcf8b
FranBlaMar <74529306+FranBlaMar@users.noreply.github.com>
2022-03-05 16:28:03 +0100

    Update CorsConfig.java

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig{
 	            @Override
 	            public void addCorsMappings(CorsRegistry registry) {
 	                registry.addMapping("/**")
-	                .allowedOrigins("http://localhost:4200")
+	                .allowedOrigins("https://franblamar.github.io/Andromeda")
 	                .allowedMethods("GET","POST", "PUT", "DELETE");
 
 	            }
