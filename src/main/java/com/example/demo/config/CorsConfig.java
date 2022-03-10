6ceaae5f4fdad4caed3adbc63c931cc828c76376
FranBlaMar <74529306+FranBlaMar@users.noreply.github.com>
2022-03-10 10:27:46 +0100

    Update CorsConfig.java

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig{
 	            @Override
 	            public void addCorsMappings(CorsRegistry registry) {
 	                registry.addMapping("/**")
-	                .allowedOrigins("https://franblamar.github.io, http://localhost:4200/")
+	                .allowedOrigins("https://franblamar.github.io" , "http://localhost:4200")
 	                .allowedMethods("GET","POST", "PUT", "DELETE");
 
 	            }
